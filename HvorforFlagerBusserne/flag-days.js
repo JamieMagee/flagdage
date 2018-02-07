2df06cbcef591e30a0d7944dfb717e03b7604ce9
Jamie Magee <jamie.magee@gmail.com>
2018-02-07 16:58:49 +0100

    Add other recurring flag days

--- HvorforFlagerBusserne/flag-days.js
+++ HvorforFlagerBusserne/flag-days.js
@@ -26,8 +26,26 @@ class FlagDay {
 }
 
 const flagDays = [
+  new FlagDay('2018-01-01', 'Nytårsdag'),
   new FlagDay('2018-02-05', 'Kronprinsesse Marys fødselsdag'),
-  new FlagDay('2018-02-06', 'Prinsesse Maries fødselsdag')
+  new FlagDay('2018-02-06', 'Prinsesse Maries fødselsdag'),
+  new FlagDay('2018-03-23', 'Nordens dag'),
+  new FlagDay('2018-04-09', 'Danmarks besættelse 1940'),
+  new FlagDay('2018-04-16', 'Dronning Margrethes fødselsdag'),
+  new FlagDay('2018-04-29', 'Prinsesse Benediktes fødselsdag'),
+  new FlagDay('2018-05-01', 'Arbejdernes kampdag'),
+  new FlagDay('2018-05-04', 'Danmarks Befrielsesdag'),
+  new FlagDay('2018-05-05', 'Danmarks befrielse 1945'),
+  new FlagDay('2018-05-06', 'Børnehjælpsdagen'),
+  new FlagDay('2018-05-09', 'Europadag', true, '🇪🇺'),
+  new FlagDay('2018-05-26', 'Kronprins Frederiks fødselsdag'),
+  new FlagDay('2018-06-05', 'Grundlovsdag'),
+  new FlagDay('2018-06-07', 'Prins Joachims fødselsdag'),
+  new FlagDay('2018-06-11', 'Prins Henriks fødselsdag'),
+  new FlagDay('2018-06-15', 'Valdemarsdag og Genforeningsdag'),
+  new FlagDay('2018-09-05', 'Danmarks Udsendte'),
+  new FlagDay('2018-10-24', 'FN-dag'),
+  new FlagDay('2018-12-25', 'Juledag')
 ];
 
 module.exports = flagDays;
