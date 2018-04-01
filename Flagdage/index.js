bf54e42cb7cd092d9ab7dc24546ec15edbddd309
Jamie Magee <jamie.magee@gmail.com>
2018-04-01 16:54:16 +0100

    Update logs

--- Flagdage/index.js
+++ Flagdage/index.js
@@ -8,6 +8,8 @@ module.exports = function run(context) {
   moment.locale('da-dk');
   const today = moment();
 
+  context.log.info(`Current date is ${today.format('D. MMMM')}`);
+
   flagDays.forEach(flagDay => {
     if (flagDay.matches(today)) {
       tweet(today, flagDay, context);
@@ -32,9 +34,9 @@ function tweet(today, flagDay, context) {
   client
     .post('statuses/update', { status: status })
     .then(function success(tweet) {
-      context.log(tweet);
+      context.log.info(tweet);
     })
     .catch(function error(error) {
-      context.log(error);
+      context.log.info(error);
     });
 }
